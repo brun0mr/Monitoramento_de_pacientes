2dce0653ea1d830922933a16f70bb233ec5eb68a
Lucas de Jesus <lucas_moreira110@hotmail.com>
2022-02-13 19:52:22 -0300

    Criação registro,login configurar com react

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
-from .views import DadosView, MedicoView, PacienteView, SensorView
+from .views import home, user_login, user_profile, user_logout, DadosView, MedicoView, PacienteView, SensorView
 
 urlpatterns = [
+    
+
+
+    path('', home),
+    path('login/', user_login, name='user_login'),
+    path('logout/', user_logout),
+    path('profile/', user_profile, name='user_profile'),
+
+
+
     path('dados', DadosView.as_view()),
     path('medico', MedicoView.as_view()),
     path('paciente', PacienteView.as_view()),
