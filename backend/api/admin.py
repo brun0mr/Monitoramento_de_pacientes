b5d0db63a43d7ab82c5273d76187624abda09c67
Lucas de Jesus <lucas_moreira110@hotmail.com>
2022-02-13 21:14:29 -0300

    Configuração da área de admin

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Medico, Paciente, Sensor, Dados
+
+
+admin.site.register(Medico)
+admin.site.register(Paciente)
+admin.site.register(Sensor)
+admin.site.register(Dados)
