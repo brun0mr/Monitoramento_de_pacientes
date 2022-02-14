367f9a338c889cc921304f79a1859dc5f6dc9e5c
pericles210 <pericles.izumi@gmail.com>
2022-02-14 20:36:42 -0300

    a

--- frontEnd/src/components/Alerts/Alerts.js
+++ frontEnd/src/components/Alerts/Alerts.js
@@ -5,12 +5,18 @@ import "./Alerts.css";
 export default function Alerts() {
     return (
       <div className="alerts_container">
-          <div className="alerts_text">
-            <p>
-                <h1><strong>Notificações</strong></h1>
-            
-            </p>
+        <div className="alerts_text">
+          <p>
+            <h1>
+              <strong>Notificações</strong>
+            </h1>
+            <div className="alerts_background">
+              <div className="alerts_row">{data}</div>
+            </div>
+          </p>
         </div>
       </div>
     );
   }
+
+  var data = ['pericles', 'izumi'];
